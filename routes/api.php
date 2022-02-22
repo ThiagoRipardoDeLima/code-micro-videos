62f0824841e8f3ee5aedbc67e46f9314cd1773c3
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2022-02-22 00:04:23 -0400

    Configurado rota CategoryController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Http\Request;
 
 /*
@@ -15,4 +14,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'Api'], function(){
+    Route::resource('categories', CategoryController::class, ['except'=>['create', 'edit']]);
 });
