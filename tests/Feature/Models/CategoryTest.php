8fdf88635f338829ed2fd30d20ea06cc8fa27d80
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2022-07-05 22:31:04 -0400

    Incluindo testes de criação do model Category

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testList()
+    {
+        factory(Category::class, 1)->create();
+
+        $categories = Category::all();
+        $this->assertCount(1, $categories);
+
+        $categoryKey = array_keys($categories->first()->getAttributes());
+
+        $attributes = ["id","name","description","is_active","deleted_at","created_at","updated_at"];
+        $this->assertEqualsCanonicalizing($attributes, $categoryKey);
+
+    }
+
+    public function testCreate()
+    {
+        $category = Category::create([
+            "name" => "test"
+        ]);
+        $category->refresh();
+
+        $this->assertEquals('test', $category->name);
+        $this->assertNull($category->description);
+        $this->assertTrue($category->is_active);
+
+        $category = Category::create([
+            "name" => "test"
+            , "description" => null
+        ]);
+        $this->assertNull($category->description);
+
+        
+        $category = Category::create([
+            "name" => "test"
+            , "description" => "test_description"
+        ]);
+        $this->assertEquals("test_description", $category->description);
+
+        $category = Category::create([
+            "name" => "test"
+            , "is_active" => false
+        ]);
+        $this->assertFalse($category->is_active);
+
+        $category = Category::create([
+            "name" => "test"
+            , "is_active" => true
+        ]);
+        $this->assertTrue($category->is_active);
+
+    }
+
+}
